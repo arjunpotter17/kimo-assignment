581bdf829efcdd3fcad9874b01bf273169d3753e
arjunpotter17 <arjunpotter17@gmail.com>
2023-04-11 09:39:22 +0530

    fixed url issue for banner

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const HeroBanner = () => {
   return (
-    <div className='flex justify-center items-center h-[30rem] md:h-[35rem] w-full bg-[url(../../public/assets/Image.png)] bg-no-repeat bg-cover'>
+    <div className='flex justify-center items-center h-[30rem] md:h-[35rem] w-full bg-[url(../assets/Image.png)] bg-no-repeat bg-cover'>
         <div className='flex flex-col justify-center items-center'>
         <p className='banner-text md:text-[90px] xl:text-[140px]'>Welcome</p>
         <p className='banner-text md:text-[90px] xl:text-[140px]'>to Hawaii</p>
